4e7cc10de6971e064b6dcc2a0f3abc7313f404e0
Vinodh Kumar Konda <53635507+vinodhkumar-k@users.noreply.github.com>
2021-02-15 09:13:38 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import './App.css';
-import { Toolbar } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles({
-  header: {
-    backgroundColor: '#232f3e',
-    height: '45px'
-  }
-});
+import Header from './components/Header/Header';
 
 function App() {
-  const styles = useStyles();
 
   return (
     <div className="App">
-      <header>
-        <Toolbar className={styles.header}>
-          <span style={{ color: 'white' }}>Movie Flix</span>
-        </Toolbar>
-      </header>
+      <Header/>
     </div>
   );
 }
