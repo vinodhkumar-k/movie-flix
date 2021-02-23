8163e07e290ff1af84974b07f88fba2bb5cb8f73
Vinodh Kumar Konda <53635507+vinodhkumar-k@users.noreply.github.com>
2021-02-23 23:00:59 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+import reportWebVitals from './reportWebVitals';
+
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import rootReducer from './store/rootReducer';
+
+export const composeEnhancers = (window && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
